1f51afdbae0b4bd65adfecef8805c571eaf057ab
Phil Ford-Hutchinson <phil_ford-hutchinson@rnli.org.uk>
2024-05-13 11:22:53 +0100

    Update sample_notebook.sql
    added new query with different clause

--- notebooks/sample_notebook.sql
+++ notebooks/sample_notebook.sql
@@ -7,3 +7,6 @@ SELECT BEACH_SLOPE, BEACH_TYPE
 FROM dev_bronze.ripcurrent.rnli_lifeguard_beaches where BEACH_SHORE_NORMAL_WAVE <=200
 
 -- COMMAND ----------
+
+SELECT BEACH_SLOPE, BEACH_TYPE
+FROM dev_bronze.ripcurrent.rnli_lifeguard_beaches where BEACH_SHORE_NORMAL_WAVE <=500
